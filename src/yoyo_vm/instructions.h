606a7ae66883ff7ecabe7676ba0428af56473994
Eric Nwogbo <ericis2007@gmail.com>
2025-04-08 15:40:22 +0100

    Full instruction set

--- src/yoyo_vm/instructions.h
+++ src/yoyo_vm/instructions.h
@@ -1,13 +1,6 @@
 #pragma once
 #include <cstdint>
 
-
-/// Type Bytes
-struct Type
-{
-    constexpr uint8_t
-};
-
 enum class OpCode : uint8_t {
     //-------1 byte Operations-----------------------
     // Integer operations, hopefully we're in two's complement
@@ -28,8 +21,22 @@ enum class OpCode : uint8_t {
     CmpEq, CmpNe,
     UCmpGt, UCmpGe, UCmpLt, UCmpLe,
     ICmpGt, ICmpGe, ICmpLt, ICmpLe,
-    // Negation Operations
+    // Bitwise Operations (no sign)
+    Shl8, Shl16, Shl32, Shl64,
+    // right shit if logical not arithmetic
+    Shr8, Shr16, Shr32, Shr64,
+    // I might remove the sized variants and perform on the entire register
+    BitAnd8, BitAnd16, BitAnd32, BitAnd64,
+    BitOr8, BitOr16, BitOr32, BitOr64,
+    BitXor8, BitXor16, BitXor32, BitXor64,
 
+    Alloca,
+    Load,
+    Store,
+    /// Offset a pointer by a value from the stack
+    PtrOff,
+    // Negation Operations
+    FNeg32, FNeg64,
     /// jump to an offset specified on the stack
     /// the offset must be a 64-byte unsigned integer
     /// the offset is specified in terms of the current function
@@ -44,12 +51,22 @@ enum class OpCode : uint8_t {
     /// but takes one byte instead of 3
     RetVoid,
     Constant8, Constant16, Constant32, Constant64,
+
+    Panic,
     //------2 byte Operations-----------------------------
     //push a value from a constant spot in the stack onto the stack top
     StackAddr,
+    /// Offset a pointer by a value specified in the next byte
+    PtrOffConst,
     /// pop the function pointer from the top of the stack
     /// and use the next n stack items as parameters of the function
     /// n is specified as the second byte of the operation
     /// it pops n values from the stack and pushes the result of the call
-    Call
+    Call,
+    //------3 byte Operations-------------------------------
+    // the next byte specifies the size of the source integer
+    // and the third byte specifies the size of the destination integer
+    Zext, Sext, Trunc,
+    FpConv,
+    FpToSi, FpToUi, UiToFp, SiToFp,
 };
